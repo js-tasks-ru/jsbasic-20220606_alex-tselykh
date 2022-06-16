86f5943d1e663f6eb46d7c6bcb0011e994264d23
Alex T <70171246+alexlost93@users.noreply.github.com>
2022-06-16 21:24:55 +0300

    max/min values and users balance functions

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,15 @@
 function showSalary(users, age) {
-  // ваш код...
+
+  const resultArr = users.filter(user => user.age <= age).map(user => `${user.name}, ${user.balance}`);
+  let resultString = '';
+
+  for (let i = 0; i < resultArr.length; i++) {
+    if (i === resultArr.length - 1) {
+      resultString += resultArr[i];
+    } else {
+      resultString += resultArr[i] + '\n';
+    }
+  }
+
+  return resultString;
 }
